54152cdd846a41e95666f45bc7351ef53993a61f
Delphi <joelmeister1209@gmail.com>
2014-04-10 13:30:07 -0400

    library for IR -- should work, need to test

--- libraries/LibIR/IR.cpp
+++ libraries/LibIR/IR.cpp
@@ -1,40 +1,55 @@
 #include <IR.h>
-
+#define FRONT 0
+#define LEFT 1
+#define RIGHT 2
+#define BACK 3
 #define RECEIVER 0
 #define SENDER 1
 #define ERROR 2
-const unsigned int msg0[] = {0x1,0x2,0x3,0x4,0x5};//replace with message name and real bytecode
-IR::IR(int p)
-	: pin(p),irRecv(p)
-{}
+
+const unsigned int IR::msg0[8] 
+    = {500,500,500,500,500,500,500,500};                                                
+const unsigned int IR::msg1[8]
+    = {500,500,500,500,500,500,500,1000};                                               
+const unsigned int IR::msg2[8]
+    = {500,500,500,500,500,500,1000,1000};                                              
+const unsigned int IR::msg3[8]
+    = {500,500,500,500,500,1000,1000,1000};                                             
+const unsigned int IR::msg4[8]
+    = {500,500,500,500,1000,1000,1000,1000};                                            
+const unsigned int IR::msg5[8]
+    = {500,500,500,1000,1000,1000,1000,1000}; 
+const unsigned int IR::msg6[8]
+    = {500,500,1000,1000,1000,1000,1000,1000}; 
+const unsigned int IR::msg7[8]
+    = {500,1000,1000,1000,1000,1000,1000,1000}; 
+const unsigned int IR::msg8[8]
+    = {1000,1000,1000,1000,1000,1000,1000,1000}; 
+
+IR::IR()
+	: irSend(),irRecv0(10),irRecv1(16),
+		irRecv2(14),irRecv3(15),
+		send_pin(5), recv_pin0(10), recv_pin1(16),
+		recv_pin2(14), recv_pin3(15)
+{
+	setup();
+/*these might change*/
+	m0_l = 72; m0_u = 86;
+	m1_l = 87; m1_u = 96;
+	m2_l = 97; m2_u = 106;
+	m3_l = 107; m3_u = 116;
+	m4_l = 117; m4_u = 126;
+	m5_l = 127; m5_u = 136;
+	m6_l = 137; m6_u = 146;
+	m7_l = 147; m7_u = 156;
+	m8_l = 157; m8_u = 166;
+}
 
 IR::~IR(){}
 
-int IR::getPin(){ return this->pin; }
-void IR::setup(int m){ 
-	this->mode = m;
-	if(m==RECEIVER){
-		irRecv.enableIRIn();
-    	Serial.println("Receiver mode");
-	}else if(m==SENDER){
-		//sender mode?
-	}else{
-		Serial.println("ERROR SELECTING IR PIN");
-	}
-}
-/*
-	sends a custom message, up to 8 bits in length (<256)
-*/
-void IR::sendCustomMessage(int data, int len){
-	if(this->mode == RECEIVER) {
-		Serial.println("You are receiver...don't send");
-		return;
-	}
-	this->irSend.sendNEC(data,len);
-}
+void IR::setup(){ }
 /*
 	sends a message from a list of preset messages
-	the message is 1 shifted left by the input value
 */
 void IR::sendMessage(int msg){
 	if(this->mode == RECEIVER) {
@@ -43,71 +58,129 @@ void IR::sendMessage(int msg){
 	}
 	switch (msg){
 		case 0:
-			this->irSend.sendNEC(0x1,8);
+			this->irSend.sendRaw((unsigned int*)IR::msg0,8,38);
 			break;
 		case 1:
-			this->irSend.sendNEC(0x2,8);
+			this->irSend.sendRaw((unsigned int*)IR::msg1,8,38);
 			break;
 		case 2:
-			this->irSend.sendNEC(0x4,8);
+			this->irSend.sendRaw((unsigned int*)IR::msg2,8,38);
 			break;
 		case 3:
-			this->irSend.sendNEC(0x8,8);
+			this->irSend.sendRaw((unsigned int*)IR::msg3,8,38);
 			break;
 		case 4:
-			this->irSend.sendNEC(0x10,8);
+			this->irSend.sendRaw((unsigned int*)IR::msg4,8,38);
 			break;
 		case 5:
-			this->irSend.sendNEC(0x20,8);
+			this->irSend.sendRaw((unsigned int*)IR::msg5,8,38);
 			break;
 		default:
-			Serial.println("unknown message..sending 0x40");
-			this->irSend.sendNEC(0x40,8);
+			this->irSend.sendRaw((unsigned int*)IR::msg8,8,38);
 			break;
 	}
+	this->resumeAll();
 	delay(100);
 }
-int IR::getMessage(){
-	if(this->mode == SENDER) {
-		Serial.println("You are sender..don't receive");
-		return -1;
-	}
-	if(this->irRecv.decode(&this->results)){
-		Serial.print("Data: ");
-		Serial.println(this->results.value, HEX);
-    	this->irRecv.resume(); // Receive the next value
-	}
 
-	return this->parseResult();//could just return results.value?
+/*
+	return direction with priority:
+		front>left>right>back
+*/
+bool IR::front(){ return fr;}
+bool IR::left(){ return le;}
+bool IR::right(){ return ri;}
+bool IR::back(){ return ba;}
+
+void IR::enableAll(){
+	irRecv0.enableIRIn();
+	irRecv1.enableIRIn();
+	irRecv2.enableIRIn();
+	irRecv3.enableIRIn();
 }
-/*might not need this function*/
-int IR::parseResult(){
-	switch(this->results.value){
-		case 0x1: /*put a signal here*/
-			break;
-		case 0x2: /*put a signal here*/
-			break;
-		case 0x4: /*put a signal here*/
-			break;
-		case 0x8: /*put a signal here*/
-			break;
-		case 0x10: /*put a signal here*/
-			break;
-		case 0x20: /*put a signal here*/
-			break;
-		default:
-			break;
+void IR::resumeAll(){
+	irRecv0.resume();
+	irRecv1.resume();
+	irRecv2.resume();
+	irRecv3.resume();
+}
+int IR::getMessage(){
+	fr = le = ba = ri = false;
+	if (irRecv0.decode(&results)) {
+		fr = true;
+		int res = getResult(irRecv0);
+		return parseResult(res, FRONT);
 	}
-
-	return this->results.value;
+	else if (irRecv1.decode(&results)) {
+		le = true;
+		int res = getResult(irRecv1);
+		return parseResult(res, LEFT);
+	}
+	else if (irRecv2.decode(&results)) {
+		ri = true;
+		int res = getResult(irRecv2);
+		return parseResult(res, RIGHT);
+	}
+	else if (irRecv3.decode(&results)) {
+		ba = true;
+	    int res = getResult(irRecv3);
+	    return parseResult(res, BACK);
+	}
+	return -1;
+}
+int IR::getResult(IRrecv &irrecv){
+    //digitalWrite(LED,LOW);
+    delay(50);
+    int sum=0;
+    for(int i=0;i<results.rawlen;i++){
+     sum+=results.rawbuf[i];
+#ifdef DEBUG
+     Serial.print(results.rawbuf[i],DEC);
+     Serial.print(" ");
+#endif
+    }
+#ifdef DEBUG
+    Serial.println(sum-results.rawbuf[0]);
+#endif
+    irrecv.resume(); // Receive the next value
+    return sum-results.rawbuf[0] ; //-results.rawbuf[results.rawlen-1];
+}
+int IR::parseResult(int res, int dir){
+	if(m0_l < res && res <= m0_u){
+		return 0;
+	}
+	if(m1_l < res && res <= m1_u){
+		return 1;
+	}
+	if(m2_l < res && res <= m2_u){
+		return 2;
+	}
+	if(m3_l < res && res <= m3_u){
+		return 3;
+	}
+	if(m4_l < res && res <= m4_u){
+		return 4;
+	}
+	if(m5_l < res && res <= m5_u){
+		return 5;
+	}
+	if(m6_l < res && res <= m6_u){
+		return 6;
+	}
+	if(m7_l < res && res <= m7_u){
+		return 7;
+	}
+	if(m8_l < res && res <= m8_u){
+		return 8;
+	}
+	return -1;
 }
 void IR::setMode(int m){
 	this->mode = m;
 	if(this->mode == RECEIVER){
-		this->irRecv.enableIRIn();
+		this->enableAll();
 	}
 	else if(this->mode == SENDER){
-		this->irSend.enableIROut(38);//kHz
 	}
 }
 int IR::getMode(){return this->mode;}
---
+++ tag_with_lib/tag_with_lib.ino
@@ -0,0 +1,92 @@
+#include <Motor.h>
+
+#include <IRremote.h>
+#include <CppList.h>
+#include <IRremoteInt.h>
+
+#include <IR.h>
+#include <NewPing.h>
+
+#define TRIG 8
+#define ECHO 9
+#define DIST 400
+#define WHEEL0 6
+#define WHEEL1 9
+#define MOTORENA 20
+#define MSG_NOT_IT 0
+#define MSG_TAG 1
+#define MSG_ACK 2
+
+int LED = 3;
+IR ir;
+NewPing sonar(TRIG,ECHO,DIST);
+Motor motor(WHEEL0, WHEEL1, MOTORENA);
+
+int IT=0;
+
+void setup(){
+	Serial.begin(9600);
+	pinMode(LED,OUTPUT);
+}
+void loop(){
+	if(IT){
+		lookForBot();
+	} else{
+		runFromBot();
+	}
+	delay(50);
+}
+
+void lookForBot(){
+	int msg = ir.getMessage();
+	if(msg == MSG_ACK){
+		IT = 0;
+		//turn around and move forward
+	}else if (msg == MSG_NOT_IT){
+		if(sonar.ping_cm() < 10 && ir.front()){
+			ir.sendMessage(MSG_TAG);
+			motor.stop();
+			delay(50);
+		}else if(ir.front()){
+			move();
+		} else if(ir.left()){
+			motor.turnLeft();
+		} else if(ir.right()){
+			motor.turnRight();
+		} else if(ir.back()){
+			motor.turnLeft();
+		} else{
+			move();
+		}
+	} else{
+		move();
+	}
+}
+/*
+	broadcast NOT_IT and wait to be tagged
+	while moving, if tagged send ACK
+*/
+void runFromBot(){
+	int msg = ir.getMessage();
+	if(msg == MSG_TAG){
+		IT = 1;
+		//send ack three times
+		for (int i = 0 ; i < 3; i++){
+			ir.sendMessage(MSG_ACK);
+			delay(50);
+		}
+	}
+	else {
+		ir.sendMessage(MSG_NOT_IT);
+		move();
+	}
+}
+/*
+	move but dont run into anything
+*/
+void move(){
+	//if wall turn left
+	motor.turnLeft();
+	//else go forward
+	motor.forward();
+}
