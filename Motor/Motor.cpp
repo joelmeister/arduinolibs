f538abd9165047d558e16f24c26cb66560bf34f0
Pradeep Gopal <ggopalp19@gmail.com>
2014-04-23 11:32:16 -0400

    Motor update

---
+++ Motor/Motor.cpp
@@ -0,0 +1,62 @@
+#include "Arduino.h"
+#include "Motor.h"
+
+Motor::Motor(int pin1, int pin2, int pin3)
+{
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(pin3, OUTPUT);
+  wheel0 = pin1;
+  wheel1 = pin2;
+  motorENA = pin3;
+}
+
+void Motor::stop() //--L
+{
+  digitalWrite(wheel0, LOW);   
+  digitalWrite(wheel1, LOW);
+  digitalWrite(motorENA, LOW);  
+}
+
+void Motor::forward() //00H
+{
+  digitalWrite(wheel0, LOW);   
+  digitalWrite(wheel1, LOW);
+  digitalWrite(motorENA, HIGH);  
+}
+
+void Motor::turnLeft() //01H
+{
+  digitalWrite(wheel0, LOW);   
+  digitalWrite(wheel1, HIGH);
+  digitalWrite(motorENA, HIGH);   
+}
+
+void Motor::turnRight() //10H
+{
+  digitalWrite(wheel0, HIGH);   
+  digitalWrite(wheel1, LOW);
+  digitalWrite(motorENA, HIGH); 
+}
+
+void Motor::backward() //11H
+{
+  digitalWrite(wheel0, HIGH);   
+  digitalWrite(wheel1, HIGH);
+  digitalWrite(motorENA, HIGH);   
+}
+
+void Motor::setForwardSpeed(int speed) //00H
+{
+  analogWrite(wheel0, speed);   
+  analogWrite(wheel1, speed);
+  digitalWrite(motorENA, HIGH);  
+}
+
+/*void Motor::turnLeft90() //10H
+{
+  turnLeft();
+
+}*/
+
+
