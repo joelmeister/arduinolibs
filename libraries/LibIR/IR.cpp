d75bded3473055978a81e7b09268745e396c8f45
joelmeister1209 <joelmeister1209@gmail.com>
2014-04-23 15:00:47 -0400

    Revert "ir lib-working on sending and receiving"
    This reverts commit c37bfca13a20badd11b2bdce93d201fa32725ef4.

--- libraries/LibIR/IR.cpp
+++ libraries/LibIR/IR.cpp
@@ -79,6 +79,7 @@ void IR::sendMessage(int msg){
 	}
 	
 	this->enableAll();
+	delay(150);
 }
 
 /*
@@ -95,15 +96,12 @@ void IR::enableAll(){
 	irRecv1.enableIRIn();
 	irRecv2.enableIRIn();
 	irRecv3.enableIRIn();
-	delay(150);
 }
 void IR::resumeAll(){
-	//enableAll();
 	irRecv0.resume();
 	irRecv1.resume();
 	irRecv2.resume();
 	irRecv3.resume();
-	delay(150);
 }
 int IR::getMessage(){
 	fr = le = ba = ri = false;
